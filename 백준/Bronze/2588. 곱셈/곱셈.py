a4063cf445a203496b1a45a616de6c54e7bcd3ff
황지연 <ghkdwldus0807@gmail.com>
2024-01-05 20:51:16 +0900

    [Bronze III] Title: 곱셈, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,10 @@
+a = int(input())
+b = list(map(int, input()))
+num3 = a * b[2]
+num4 = a * b[1]
+num5 = a * b[0]
+num6 = num3 + num4 * 10 + num5 * 100
+print(num3)
+print(num4)
+print(num5)
+print(num6)
