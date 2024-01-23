bdefbaec665f85a2f040387ef41e5f31bfc7b994
황지연 <ghkdwldus0807@gmail.com>
2024-01-23 23:10:27 +0900

    [level 1] Title: 약수의 합, Time: 0.10 ms, Memory: 67.8 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int n) {
         int sum = 0;
-        for(int i = 1; i<n+1; i++){
+        for(int i = 1; i<n+1/2; i++){
             if(n%i == 0 ){
                 sum = sum + i;
             }
         }
-        int answer = sum;
+        int answer = sum+n;
         return answer;
     }
 }
