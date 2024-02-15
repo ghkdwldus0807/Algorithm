e3ba99d723e6e1ceec3952191647b6bbf40405ca
황지연 <ghkdwldus0807@gmail.com>
2024-02-15 14:19:03 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 110.26 ms, Memory: 27.7 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,8 +1,6 @@
 def solution(arr):
     answer = []
     for i in arr :
-        if i not in answer:
-            answer.append(i)
-        elif i in answer and answer[-1]!=i :
+        if len(answer) == 0 or answer[-1]!=i:
             answer.append(i)
     return answer
