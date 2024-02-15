00b1c69661802c4026c6e29a57605a5b13ef8a74
황지연 <ghkdwldus0807@gmail.com>
2024-02-15 14:50:19 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.py
@@ -1,18 +1,11 @@
 def solution(n):
     answer = 0
-    ternary = []
-    answer_list = []
-    a = n
-    while a>0 :
-        ternary.insert(0,a%3)
-        a = a//3
+    tmp = ''
     
-    ternary.reverse()
+    while n:
+        tmp += str(n%3)
+        n = n//3
     
-    for i in range (0, len(ternary)) :
-        if ternary[i] == 0 :
-            continue
-        else :
-            answer_list.append(ternary[i]*3**(len(ternary)-i-1))
-    answer = sum(answer_list)
+    answer = int(tmp, 3)
+    
     return answer
