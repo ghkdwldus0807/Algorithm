69a79822a4824c274a15e3d6fbcf734a12ea476e
황지연 <ghkdwldus0807@gmail.com>
2024-02-17 15:17:02 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.32 ms, Memory: 9.99 MB -BaekjoonHub

---
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.py
@@ -0,0 +1,11 @@
+def solution(numbers):
+    answer = []
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i]+numbers[j] in answer :
+                continue
+            answer.append(numbers[i]+numbers[j])
+    
+    answer.sort()
+    return answer
