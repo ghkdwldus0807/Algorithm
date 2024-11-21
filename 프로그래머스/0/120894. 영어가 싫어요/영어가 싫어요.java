148e814115a700615ce15c51b475853c171d6e94
황지연 <ghkdwldus0807@gmail.com>
2024-11-21 20:46:40 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long solution(String numbers) {
+        long answer = 0;
+        String result = numbers.replace("zero","0").replace("one","1").replace("two","2")
+            .replace("three","3").replace("four","4").replace("five","5").replace("six","6")
+            .replace("seven","7").replace("eight","8").replace("nine","9");
+        
+        answer = Long.parseLong(result);
+        return answer;
+    }
+}
