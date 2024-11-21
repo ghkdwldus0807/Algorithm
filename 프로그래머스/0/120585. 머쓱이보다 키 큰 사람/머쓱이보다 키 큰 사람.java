5a4f57721283c3d62df665a60a5018fad8a55f75
황지연 <ghkdwldus0807@gmail.com>
2024-11-21 20:42:33 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.03 ms, Memory: 75.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int n : array){
+            if(n>height)
+                answer+=1;
+        }
+
+        return answer;
+    }
+}
