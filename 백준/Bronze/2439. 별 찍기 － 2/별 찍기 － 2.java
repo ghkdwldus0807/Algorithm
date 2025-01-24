b0f464a126651c5fef21a20ac57b734c2695758e
황지연 <ghkdwldus0807@gmail.com>
2025-01-24 17:37:27 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 112 ms, Memory: 14464 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+public class Main  {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int num = Integer.parseInt(br.readLine());
+		
+		String str = "*";
+		String blank =" ";
+		
+		for(int i=1; i<=num; i++) {
+			System.out.print(blank.repeat(num-i));
+			System.out.println(str.repeat(i));
+		}
+		
+	}//main method
+
+}
