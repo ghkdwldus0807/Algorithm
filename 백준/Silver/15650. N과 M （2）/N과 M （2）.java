e4344c2b07959c5a3c89a244f9e2e4d4b5c590a7
황지연 <ghkdwldus0807@gmail.com>
2025-02-10 16:45:01 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15812 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+
+public class Main {
+	
+	static int n,m;
+	static int[] num;
+	static StringBuilder sb;
+
+	public static void main(String[] args) throws IOException{
+		
+		sb = new StringBuilder();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		num = new int [m];
+		
+		set(1,0);
+		
+		System.out.println(sb);
+		
+
+	}
+	
+	
+	static void set(int start, int index) {
+		
+		if(index == m) {
+			for(int i : num)
+				sb.append(i+" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start; i<=n; i++) {
+			num[index] = i;
+			set(start+1,index+1);
+			start ++;
+			
+		}
+	}
+
+}
