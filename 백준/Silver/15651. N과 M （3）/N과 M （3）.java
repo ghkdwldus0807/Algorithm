105b3a7358ee9c2fc98076e98bc21a0270adf368
황지연 <ghkdwldus0807@gmail.com>
2025-02-10 16:16:02 +0900

    [Silver III] Title: N과 M (3), Time: 520 ms, Memory: 209116 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+	
+	static int n,m;
+	static boolean[] visited; 
+	static int[] num;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		sb = new StringBuilder();
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		num = new int [m];
+		visited = new boolean[n+1];
+		
+		dfs(0);
+		
+		System.out.println(sb);
+
+
+	}//main 
+	
+	static void dfs(int index) {
+		
+		if(index==m) {
+			for(int i: num)
+				sb.append(i+" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=1; i<=n; i++) {
+			num[index] = i;
+			dfs(index+1);
+			
+		}
+	
+		
+		
+	}
+
+}
