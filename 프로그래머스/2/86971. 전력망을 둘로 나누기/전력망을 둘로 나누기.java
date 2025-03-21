0ededf34bc41b4529ebf4eb88748a2849e80bf19
황지연 <ghkdwldus0807@gmail.com>
2025-03-21 13:17:25 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 0.38 ms, Memory: 98.4 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,81 +1,51 @@
+/*
+dfs를 이용하여 자식 노드 수 계산하여 n과 차이를 비교  -> 해당 간선을 끊는 효과 
+*/
 import java.util.*;
 
-
 class Solution {
-    
-    static boolean [] visited;
-    static ArrayList<Integer> [] tree ;
-    static Queue<Integer> queue = new ArrayDeque<>();
-    static int count = 0; 
+    int min,N;
+    ArrayList<Integer>[] tree ;
+    boolean [] visited ;
     
     public int solution(int n, int[][] wires) {
+        int answer = -1;
+        min = n;
+        N = n;
+        visited = new boolean [N+1];
         
-        int answer = Integer.MAX_VALUE;
-        tree = new ArrayList[n+1];
-        visited = new boolean[n+1];
+        //1. 트리 만들기
+        tree = new ArrayList [n+1];
         
-        for(int t=0; t<n-1; t++){
-            
-            for(int i=1; i<=n; i++){
-                tree[i] = new ArrayList<Integer>();
-            } // tree 초기화
-            
-            Arrays.fill(visited,false); //visited 초기화
-            queue.clear();
-            
-            for(int i=0; i<n-1; i++){
-                if(i == t) continue; //t번째 wire는 연결하지 않기 
-                
-                int v1 = wires[i][0];
-                int v2 = wires[i][1];
-                // 양방향 연결 
-                tree[v1].add(v2);
-                tree[v2].add(v1);
-                
-                
-            }//tree 연결 
-            
-            for(int i=1; i<=n; i++){
-
-                
-                if(visited[i])
-                    continue;
-                else {
-                    count = 0;
-                    dfs(i);
-                    queue.add(count);
-                    
-                }
-                
-            }//dfs 수행 
-            
-            int n1 = queue.poll();
-            int n2 = queue.poll();
-            answer = Math.min (Math.abs(n1-n2), answer);
-            
-        }
-
-        
-        return answer;
-        
-    }//메인
-    
-    
-    //dfs 
-    
-    public void dfs(int start){
-        
-        visited[start] = true; 
-        count ++; 
-        
-        for(int node : tree[start]){
-            if(!visited[node]) 
-                dfs(node);
+        for(int i=1; i<=n; i++){
+            tree[i] = new ArrayList<>();
         }
         
+        for(int[] wire : wires){
+            int v1 = wire[0];
+            int v2 = wire[1];
+            
+            tree[v1].add(v2);
+            tree[v2].add(v1);
 
+        }
+        
+        dfs(1);
+        
+        return min;
+    }//sol
+    
+    public int dfs(int node){
+        int cnt  = 1; 
+        visited[node] = true;
+        
+        for(int child : tree[node]){
+            if(visited[child]) continue;
+            cnt += dfs(child);
+        }
+        
+        min = Math.min(min, Math.abs(cnt - (N-cnt)));
+        
+        return cnt;
     }
-    
-    
-    
 }
