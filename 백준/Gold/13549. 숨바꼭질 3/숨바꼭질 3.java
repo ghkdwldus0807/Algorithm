ba2a5f490c6fe0bbf7b6c8f68de0a1989679ce89
황지연 <ghkdwldus0807@gmail.com>
2025-03-24 10:55:49 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 16016 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+	static int start, end; 
+	static int [] dist = new int [100001];
+
+	
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		start = Integer.parseInt(st.nextToken());
+		end = Integer.parseInt(st.nextToken());
+		
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		
+		bfs(start,end);
+		
+		System.out.println(dist[end]);
+		
+		
+
+	}//main 
+	
+	
+	
+	//0-1 bfs
+	static void bfs(int start, int end) {
+		
+		Deque<Integer> dq = new ArrayDeque<>();
+		
+		dist[start] = 0;
+		dq.add(start);
+		
+		while(!dq.isEmpty()) {
+			
+			int current = dq.poll();
+			
+			if(current == end)
+				return;
+			
+			//dist 범위 조건을 꼭 넣어줘야함 아니면 범위를 넘어서 -1(음의 인덱스)나 100000을 넘을 수 있음..
+			
+			if(current*2 <= 100000 && dist[current*2] > dist[current]) {
+				dist[current*2] = dist[current];
+				dq.addFirst(current*2);
+			}
+			
+			if(current-1 > -1 && dist[current-1] > dist[current]+1) {
+				dist[current-1] = dist[current] + 1;
+				dq.addLast(current-1);
+				
+			}
+			
+			if(current+1 <= 100000 && dist[current+1] > dist[current]+1) {
+				dist[current+1] = dist[current]+1;
+				dq.addLast(current+1);
+				
+			}
+			
+
+		}
+		
+	}//bfs
+	
+	
+}
