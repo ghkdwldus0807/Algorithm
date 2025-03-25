5dabbc88f6d6b6a7dc574ba1092656d827989127
황지연 <ghkdwldus0807@gmail.com>
2025-03-25 17:45:13 +0900

    [Silver IV] Title: 수 찾기, Time: 1088 ms, Memory: 52100 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,68 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, M;
+	static int[] arr;
+
+	public static void main(String[] args) throws IOException{
+
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		
+		N = Integer.parseInt(br.readLine());
+		arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i=0 ; i<N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		M = Integer.parseInt(br.readLine());
+		
+		st = new StringTokenizer(br.readLine());
+		
+		Arrays.sort(arr);
+		
+		for(int i=0; i<M; i++) {
+			
+			int target = Integer.parseInt(st.nextToken());
+			
+			if(isExisted(arr, target))
+				System.out.println(1);
+			else
+				System.out.println(0);
+		}
+
+		
+	}//main 
+	
+	
+	//binary search
+	static boolean isExisted(int[] arr, int target) {
+		
+		int start = 0;
+		int end = arr.length - 1; 
+		
+		while(start<=end) {
+			int mid = (start+end)/2;
+			
+			if(arr[mid] == target)
+				return true;
+			
+			if(arr[mid]<target) {
+				start = mid+1;
+			}
+			else {
+				end = mid - 1; 
+			}
+			
+		}
+		
+		return false;
+		
+		
+	}
+	
+
+}
