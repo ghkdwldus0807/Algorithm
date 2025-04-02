c699e8a3f5d80d8092b76f988d62acbe0dcbbc8d
황지연 <ghkdwldus0807@gmail.com>
2025-04-02 15:30:55 +0900

    [Gold V] Title: 물병, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int N, K, count;
+
+	public static void main(String[] args) throws IOException{
+		
+		BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken()); 
+		//입력
+		
+		while(true) {
+			
+			int oneCount = Integer.bitCount(N); //1의 개수 
+			
+			if(oneCount<=K) {
+				break;
+			}
+			
+			int idx = Integer.numberOfTrailingZeros(N);
+			N += (int)Math.pow(2, idx);
+			count += (int)Math.pow(2, idx);
+			
+		}
+		
+		System.out.println(count);
+
+	}//main
+
+}
