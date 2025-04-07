45860f91faf81833c7c09b1ebac6170643b4fb1a
황지연 <ghkdwldus0807@gmail.com>
2025-04-07 15:29:05 +0900

    [Silver V] Title: 막대기, Time: 104 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,6 @@
+import java.io.*;
+public class Main {
+	public static void main(String[] args) throws IOException{
+		System.out.println(Integer.bitCount(Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine())));
+	}
+}
